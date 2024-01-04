c00d7ceaa40194c63b05229f83f59f285f38ef83
Lorenzo Stella <lorenzostella@gmail.com>
2024-01-04 23:50:24 +0100

    keymap tree toggle

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -48,6 +48,8 @@ return {
         },
         config = function()
             require("nvim-tree").setup {}
+            local api = require("nvim-tree.api")
+            vim.keymap.set("n", "<leader>e", api.tree.toggle)
         end,
     }
 }
