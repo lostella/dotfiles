4a0e094c55c0458005c77b6a6c919d05ba3cfb55
Lorenzo Stella <lorenzostella@gmail.com>
2024-01-13 22:35:39 +0100

    update colorscheme and fix colors on tmux

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -12,12 +12,11 @@ return {
         end
     },
     {
-        "folke/tokyonight.nvim",
-        lazy = false,
+        "catppuccin/nvim",
+        name = "catppuccin",
         priority = 1000,
-        opts = {},
         config = function()
-            vim.cmd[[colorscheme tokyonight]]
+            vim.cmd[[colorscheme catppuccin]]
         end,
     },
     {
